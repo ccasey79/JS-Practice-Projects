8cf3da3d55b206b1bc3c4c83288efe2359c17916
christian <christiancasey969@gmail.com>
2017-04-21 18:18:37 +0100

    ES6 Const exercise

--- es6/main.js
+++ es6/main.js
@@ -16,12 +16,24 @@ window.onload = function() {
 // DEFAULT PARAMETERS
 
   function topNinja(name="Burt", colour="ginger", weapon="snake") {
-
     console.log(name + " has " + colour +  " hair and he has a deadly " +  weapon);
 
   }
 
   topNinja();
+
+  // CONSTANTS
+
+  const pi = 3.142;          // this const value cannot be changed, ignores the function const.
+
+    function calcArea(r) {
+      const pi = 10;           // const value can only be changed inside the function
+      console.log("The area calculated inside the calcArea function is " + pi * r * r );
+    }
+
+  calcArea(6);
+  console.log("The value of " + pi + " outside the calcArea function");
+
 
 
 
