c9f99878e0fad9611a8caefc8a0ce500658d0528
christian <christiancasey969@gmail.com>
2017-04-29 16:35:45 +0100

    For loops & if statements added to todo list

--- general/todosObjects.js
+++ general/todosObjects.js
@@ -2,7 +2,20 @@
 var todoList = {
   todos: [],
   displayTodos: function() {
-    console.log('My Todos: ', this.todos);
+
+    if (this.todos.length === 0) {
+      console.log('The todos list is empty!');
+    } else {
+        console.log('My Todos: ');
+        for(var i = 0; i < this.todos.length; i++){
+
+        if ( this.todos[i].completed === true ) {
+          console.log('(x)', this.todos[i].todoText);
+        } else {
+          console.log('()', this.todos[i].todoText);
+        }
+      }
+    }
   },
   addTodo: function(todoText) {
     this.todos.push({
@@ -24,5 +37,4 @@ var todoList = {
     todo.completed = !todo.completed;
     this.displayTodos();
   }
-
 }; 
