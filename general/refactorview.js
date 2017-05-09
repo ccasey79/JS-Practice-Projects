2f49abb70391ba5db509a34964be1660095b7a08
christian <christiancasey969@gmail.com>
2017-05-09 13:36:50 +0100

    refactorview.js added to show concise code

--- general/refactorview.js
+++ general/refactorview.js
@@ -1,41 +1,22 @@
 
+
 var todoList = {
   todos: [],
-  // displayTodos: function() {
-  //
-  //   if (this.todos.length === 0) {
-  //     console.log('The todos list is empty!');
-  //   } else {
-  //       console.log('My Todos: ');
-  //       for(var i = 0; i < this.todos.length; i++){
-  //
-  //       if ( this.todos[i].completed === true ) {
-  //         console.log('(x)', this.todos[i].todoText);
-  //       } else {
-  //         console.log('()', this.todos[i].todoText);
-  //       }
-  //     }
-  //   }
-  // },
   addTodo: function(todoText) {
     this.todos.push({
       todoText: todoText,
       completed: false
     });
-    // this.displayTodos();
   },
   changeTodo: function(position, todoText) {
     this.todos[position].todoText = todoText;
-    // this.displayTodos();
   },
   deleteTodo: function(position) {
     this.todos.splice(position, 1);
-    // this.displayTodos();
   },
   toggleCompleted: function(position) {
     var todo = this.todos[position];
     todo.completed = !todo.completed;
-    // this.displayTodos();
   },
   // Using true & false combinations
   toggleAll: function() {
@@ -60,14 +41,11 @@ var todoList = {
         this.todos[i].completed = true;
       }
     }
-    // this.displayTodos();
   }
 };
 
-// CLICK HANDLERS
+// CLICK HANDLERS - when user clicks & inputs data
 var handlers = {
-  // displayTodos: function() {
-  //   todoList.displayTodos();
 
   addTodo: function() {
     var addTodoTextInput = document.getElementById('addTodoTextInput');     // input box - connect via getElementById
@@ -100,14 +78,6 @@ var handlers = {
     view.displayTodos();
   }
 };
-
-// Creating the list items - create li & ul, then append li to the ul.
-
-// var todoLi = document.createElement('li');
-// var todoUl = document.querySelector('ul');
-//
-// todoUl.appendChild(todoLi);
-
 
 // VIEWING THE TODOLIST - dynamically adding the todolist to the DOM.
 
