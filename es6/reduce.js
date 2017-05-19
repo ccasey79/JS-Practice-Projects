54bc59a229436081e4c1b967315014227a18f4e1
christian <christiancasey969@gmail.com>
2017-05-19 14:17:59 +0100

    es6 - helper array, const, let practice

--- es6/reduce.js
+++ es6/reduce.js
@@ -13,3 +13,32 @@ primaryColours.reduce(function(previous, primaryColour) {
 }, []);
 
 /// returns - ['red', 'blue', 'green'];
+
+
+///ANOTHER EXAMPLE
+
+var numbers = [10,20,30];
+
+var sum = 0;
+
+numbers.reduce(function(sum, number) {
+  return sum + number;
+}, 0);
+
+// return 60
+
+/// ANOTHER EXAMPLE
+
+// Use the 'reduce' helper to find the sum of all the distances traveled.  Assign the result to the variable 'totalDistance'
+
+var trips = [{ distance: 34 }, { distance: 12 } , { distance: 1 }];
+
+var sum = 0;
+
+var totalDistance = trips.reduce(function(sum, trip) {
+    return sum + trip.distance;
+}, 0);
+
+totalDistance;
+
+// returns total distance amount
