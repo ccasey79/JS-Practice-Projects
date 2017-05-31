b7ffa0c4ac3988f3ab88c8b06743989338e25703
christian <christiancasey969@gmail.com>
2017-05-31 11:47:09 +0100

    practice adding lists dynamically to the dom

--- general/exercise.js
+++ general/exercise.js
@@ -2,39 +2,39 @@
 
 var countryList = {
   countries: [],
-  displayCountries: function() {
-      if(this.countries.length === 0 ) {
-        console.log('The country list is empty');
-      } else {
-        console.log('My countries: ');
-          for( var i = 0; i < this.countries.length; i++ ) {
-            if(this.countries[i].completed === true ) {
-              console.log('(x)', this.countries[i].textCountry);
-            } else {
-              console.log('()', this.countries[i].textCountry);
-            }
-         }
-      }
-  },
+  // displayCountries: function() {
+  //     if(this.countries.length === 0 ) {
+  //       console.log('The country list is empty');
+  //     } else {
+  //       console.log('My countries: ');
+  //         for( var i = 0; i < this.countries.length; i++ ) {
+  //           if(this.countries[i].completed === true ) {
+  //             console.log('(x)', this.countries[i].textCountry);
+  //           } else {
+  //             console.log('()', this.countries[i].textCountry);
+  //           }
+  //        }
+  //     }
+  // },
   addToCountry: function(textCountry) {
     this.countries.push({
       textCountry: textCountry,
       completed: false
     });
-    this.displayCountries();
+    // this.displayCountries();
   },
   changeCountry: function(position, textCountry) {
     this.countries[position].textCountry = textCountry;
-    this.displayCountries();
+    // this.displayCountries();
   },
   deleteCountry: function(position) {
     this.countries.splice(position, 1);
-    this.displayCountries();
+    // this.displayCountries();
   },
   toggleCompleted: function(position) {
     var country = this.countries[position];
     country.completed = !country.completed;
-    this.displayCountries();
+    // this.displayCountries();
   },
   toggleAll: function() {
     var completedCountries = 0;
@@ -57,7 +57,7 @@ var countryList = {
             this.countries[i].completed = true;
           }
         }
-      this.displayCountries();
+      // this.displayCountries();
    }
 };
 
@@ -74,16 +74,14 @@ var countryList = {
 // Handlers object - for events on objects
 
 var handlers = {
-  displayCountries: function() {
-    countryList.displayCountries();
-  },
-  toggleAll: function() {
-    countryList.toggleAll();
-  },
+  // displayCountries: function() {
+  //   countryList.displayCountries();
+  // },
   addToCountry: function() {
     var addCountryTextInput = document.getElementById('addCountryTextInput');
     countryList.addToCountry(addCountryTextInput.value);
     addCountryTextInput.value = "";
+    view.displayCountries();
   },
   changeCountry: function() {
     var changeCountryPositionInput = document.getElementById('changeCountryPositionInput');
@@ -91,5 +89,45 @@ var handlers = {
     countryList.changeCountry(changeCountryPositionInput.valueAsNumber, changeCountryTextInput.value);
     changeCountryPositionInput.value = "";
     changeCountryTextInput.value = "";
+    view.displayCountries();
+  },
+  deleteCountry: function() {
+    var deleteCountryPositionInput = document.getElementById('deleteCountryPositionInput');
+    countryList.deleteCountry(deleteCountryPositionInput.valueAsNumber);
+    deleteCountryPositionInput.value = "";
+    view.displayCountries();
+  },
+  toggleCompleted: function() {
+    var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+    countryList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+    toggleCompletedPositionInput.valueAsNumber = "";
+    view.displayCountries();
+  },
+  toggleAll: function() {
+    countryList.toggleAll();
+    view.displayCountries();
+  }
+};
+
+/// View object - displaying the list dynamically into the document
+
+var view = {
+    displayCountries: function() {
+      var countriesUl = document.querySelector('ul');
+      countriesUl.innerHTML = "";
+      for( var i = 0; i < countryList.countries.length; i++) {
+        var countriesLi = document.createElement('li');
+        var country = countryList.countries[i];
+        var countryTextWithCompletion = "";
+
+        if (country.completed === true ) {
+            countryTextWithCompletion = ('x') + country.textCountry;
+        } else {
+            countryTextWithCompletion = (' ') + country.textCountry;
+        }
+
+        countriesLi.textContent = countryTextWithCompletion;
+        countriesUl.appendChild(countriesLi);
+    }
   }
 };
