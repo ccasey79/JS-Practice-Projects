34bd3f5d59aac921ac0d82366e475b12069f365e
christian <christiancasey969@gmail.com>
2017-11-09 13:24:30 +0000

    modal - markup & css done

---
+++ modal/main.js
@@ -0,0 +1,10 @@
+console.log('JS loaded!');
+
+// Get modal element 
+var modal = document.getElementById('simpleModal');
+
+// Get the Modal Open Button
+var modalBtn = document.getElementById('modalBtn');
+
+// Get the Modal Close button
+var closeBtn = document.getElementById('closeBtn');
